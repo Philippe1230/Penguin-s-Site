de72f717cdd0d6e5bcf8e7c618e4f7ed03c257e2
Philippe Gonçalves <philippeag40@gmail.com>
2026-02-05 11:31:58 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {
   vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import jsconfigPaths from 'vite-jsconfig-paths';
@@ -13,19 +12,33 @@ import rehypePrism from '@mapbox/rehype-prism';
 
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
+
   build: {
     assetsInlineLimit: 1024,
   },
+
   server: {
     port: 7777,
   },
+
+  ssr: {
+    noExternal: true,
+  },
+
   plugins: [
     mdx({
-      rehypePlugins: [[rehypeImgSize, { dir: 'public' }], rehypeSlug, rehypePrism],
-      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [
+        [rehypeImgSize, { dir: 'public' }],
+        rehypeSlug,
+        rehypePrism,
+      ],
+      remarkPlugins: [
+        remarkFrontmatter,
+        remarkMdxFrontmatter,
+      ],
       providerImportSource: '@mdx-js/react',
     }),
-    remixCloudflareDevProxy(),
+
     remix({
       routes(defineRoutes) {
         return defineRoutes(route => {
@@ -33,6 +46,7 @@ export default defineConfig({
         });
       },
     }),
+
     jsconfigPaths(),
   ],
 });
